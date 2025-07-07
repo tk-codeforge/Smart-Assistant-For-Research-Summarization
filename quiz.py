74bd8d3d5827dcf10124b1f1333e054da9da1253
tk-codeforge <tanmay.kumar54512@gmail.com>
2025-07-07 20:28:06 +0530

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,22 @@
+from langchain.chat_models import SmartAssistant
+
+llm = SmartAssistant(temperature=0)
+
+def generate_questions(doc_text):
+    prompt = f"Generate 3 logic or comprehension questions from this document:\n\n{doc_text[:3000]}"
+    response = llm.predict(prompt)
+    return response.strip().split("\n")
+
+def evaluate_answer(question, user_answer, doc_text):
+    prompt = f"""
+Question: {question}
+User's Answer: {user_answer}
+
+Evaluate the answer based only on the document below:
+{doc_text[:3000]}
+
+Reply with:
+- Correct/Incorrect
+- Explanation (cite the relevant part of the document)
+"""
+    return llm.predict(prompt)
