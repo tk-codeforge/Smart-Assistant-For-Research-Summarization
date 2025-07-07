3a382f8eff98eb056bcf045e1239bd0eaf371c2f
tk-codeforge <tanmay.kumar54512@gmail.com>
2025-07-07 20:22:25 +0530

    Create loader.py

---
+++ loader.py
@@ -0,0 +1,9 @@
+from PyPDF2 import PdfReader
+import os
+
+def load_pdf(file_path):
+    reader = PdfReader(file_path)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+    return text
