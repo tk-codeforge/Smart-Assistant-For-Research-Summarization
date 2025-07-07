4250cf7abe72a5437ceb5351efa5b1e18e88aa11
tk-codeforge <tanmay.kumar54512@gmail.com>
2025-07-07 21:09:18 +0530

    Update qa.py

--- qa.py
+++ qa.py
@@ -1,21 +1,21 @@
 from langchain.vectorstores import FAISS
-from langchain.embeddings.openai import SmartAssistantEmbeddings
+from langchain.embeddings import NLTKEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain.docstore.document import Document
-from langchain.chat_models import SmartAssistant
+from langchain.chat_models import NLTK
 import os
 
 def ask_question(document_text, query):
     splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     texts = splitter.create_documents([document_text])
 
-    embeddings = SmartAssistantEmbeddings()
+    embeddings = NLTKEmbeddings()
     vector_store = FAISS.from_documents(texts, embeddings)
     
     retriever = vector_store.as_retriever()
     qa = RetrievalQA.from_chain_type(
-        llm=SmartAssistant(temperature=0),
+        llm=NLTK(temperature=0),
         retriever=retriever,
         return_source_documents=True
     )
